d7d7381fa0ce1167d8c4f7dfc6c520b68c4ecb72
Sonny Najar <rodavlasrajan@gmail.com>
2018-06-26 08:13:14 -0700

    mt adv quest 5,6,7

---
+++ mt/tsmt_particle.rkt
@@ -0,0 +1,422 @@
+#reader(lib"read.ss""wxme")WXME0108 ## 
+#|
+   This file uses the GRacket editor format.
+   Open this file in DrRacket version 6.10 or later to read it.
+
+   Most likely, it was created by saving a program in DrRacket,
+   and it probably contains a program with non-text elements
+   (such as images or comment boxes).
+
+            http://racket-lang.org/
+|#
+ 33 7 #"wxtext\0"
+3 1 6 #"wxtab\0"
+1 1 8 #"wximage\0"
+2 0 8 #"wxmedia\0"
+4 1 34 #"(lib \"syntax-browser.ss\" \"mrlib\")\0"
+1 0 36 #"(lib \"cache-image-snip.ss\" \"mrlib\")\0"
+1 0 68
+(
+ #"((lib \"image-core.ss\" \"mrlib\") (lib \"image-core-wxme.rkt\" \"mr"
+ #"lib\"))\0"
+) 1 0 16 #"drscheme:number\0"
+3 0 44 #"(lib \"number-snip.ss\" \"drscheme\" \"private\")\0"
+1 0 36 #"(lib \"comment-snip.ss\" \"framework\")\0"
+1 0 93
+(
+ #"((lib \"collapsed-snipclass.ss\" \"framework\") (lib \"collapsed-sni"
+ #"pclass-wxme.ss\" \"framework\"))\0"
+) 0 0 43 #"(lib \"collapsed-snipclass.ss\" \"framework\")\0"
+0 0 19 #"drscheme:sexp-snip\0"
+0 0 29 #"drscheme:bindings-snipclass%\0"
+1 0 101
+(
+ #"((lib \"ellipsis-snip.rkt\" \"drracket\" \"private\") (lib \"ellipsi"
+ #"s-snip-wxme.rkt\" \"drracket\" \"private\"))\0"
+) 2 0 88
+(
+ #"((lib \"pict-snip.rkt\" \"drracket\" \"private\") (lib \"pict-snip.r"
+ #"kt\" \"drracket\" \"private\"))\0"
+) 0 0 55
+#"((lib \"snip.rkt\" \"pict\") (lib \"snip-wxme.rkt\" \"pict\"))\0"
+1 0 34 #"(lib \"bullet-snip.rkt\" \"browser\")\0"
+0 0 25 #"(lib \"matrix.ss\" \"htdp\")\0"
+1 0 22 #"drscheme:lambda-snip%\0"
+1 0 29 #"drclickable-string-snipclass\0"
+0 0 26 #"drracket:spacer-snipclass\0"
+0 0 57
+#"(lib \"hrule-snip.rkt\" \"macro-debugger\" \"syntax-browser\")\0"
+1 0 26 #"drscheme:pict-value-snip%\0"
+0 0 45 #"(lib \"image-snipr.ss\" \"slideshow\" \"private\")\0"
+1 0 38 #"(lib \"pict-snipclass.ss\" \"slideshow\")\0"
+2 0 55 #"(lib \"vertical-separator-snip.ss\" \"stepper\" \"private\")\0"
+1 0 18 #"drscheme:xml-snip\0"
+1 0 31 #"(lib \"xml-snipclass.ss\" \"xml\")\0"
+1 0 21 #"drscheme:scheme-snip\0"
+2 0 34 #"(lib \"scheme-snipclass.ss\" \"xml\")\0"
+1 0 10 #"text-box%\0"
+1 0 32 #"(lib \"text-snipclass.ss\" \"xml\")\0"
+1 0 1 6 #"wxloc\0"
+          0 0 63 0 1 #"\0"
+0 75 1 #"\0"
+0 10 90 -1 90 -1 3 -1 0 1 0 1 0 0 0 0 0 0 0 0 0 0 0 255 255 255 1 -1 0 9
+#"Standard\0"
+0 75 12 #"Courier New\0"
+0 11 90 -1 90 -1 3 -1 0 1 0 1 0 0 0 0 0 0 0 0 0 0 0 255 255 255 1 -1 2 1
+#"\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 -1 -1 -1 0 0 0 0 0 0 1 1 1 1 1 1 0 0 0 0 0 0 -1 -1 2 24
+#"framework:default-color\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 -1 -1 -1 0 0 0 0 0 0 0 0 0 1 1 1 0 0 0 0 0 0 -1 -1 2 1
+#"\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 -1 -1 -1 0 0 0 0 0 0 0 0 0 1 1 1 150 0 150 0 0 0 -1 -1 2 15
+#"text:ports out\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 150 0 150 0 0 0 -1 -1 2 1
+#"\0"
+0 -1 1 #"\0"
+1.0 0 -1 -1 93 -1 -1 -1 0 0 0 0 0 0 0 0 0 1.0 1.0 1.0 255 0 0 0 0 0 -1
+-1 2 15 #"text:ports err\0"
+0 -1 1 #"\0"
+1 0 -1 -1 93 -1 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 255 0 0 0 0 0 -1 -1 2 1
+#"\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 -1 -1 -1 0 0 0 0 0 0 0 0 0 1 1 1 0 0 175 0 0 0 -1 -1 2 17
+#"text:ports value\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 0 0 175 0 0 0 -1 -1 2 1
+#"\0"
+0 -1 1 #"\0"
+1.0 0 92 -1 -1 -1 -1 -1 0 0 0 0 0 0 0 0 0 1.0 1.0 1.0 34 139 34 0 0 0 -1
+-1 2 27 #"Matching Parenthesis Style\0"
+0 -1 1 #"\0"
+1.0 0 92 -1 -1 -1 -1 -1 0 0 0 0 0 0 0 0 0 1.0 1.0 1.0 34 139 34 0 0 0 -1
+-1 2 1 #"\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 38 38 128 0 0 0 -1 -1 2 37
+#"framework:syntax-color:scheme:symbol\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 38 38 128 0 0 0 -1 -1 2 38
+#"framework:syntax-color:scheme:keyword\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 38 38 128 0 0 0 -1 -1 2 1
+#"\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 194 116 31 0 0 0 -1 -1 2
+38 #"framework:syntax-color:scheme:comment\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 194 116 31 0 0 0 -1 -1 2 1
+#"\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 41 128 38 0 0 0 -1 -1 2 37
+#"framework:syntax-color:scheme:string\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 41 128 38 0 0 0 -1 -1 2 35
+#"framework:syntax-color:scheme:text\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 41 128 38 0 0 0 -1 -1 2 39
+#"framework:syntax-color:scheme:constant\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 41 128 38 0 0 0 -1 -1 2 1
+#"\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 132 60 36 0 0 0 -1 -1 2 49
+#"framework:syntax-color:scheme:hash-colon-keyword\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 132 60 36 0 0 0 -1 -1 2 42
+#"framework:syntax-color:scheme:parenthesis\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 132 60 36 0 0 0 -1 -1 2 1
+#"\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 255 0 0 0 0 0 -1 -1 2 36
+#"framework:syntax-color:scheme:error\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 255 0 0 0 0 0 -1 -1 2 1
+#"\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 0 0 0 0 0 0 -1 -1 2 36
+#"framework:syntax-color:scheme:other\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 0 0 0 0 0 0 -1 -1 2 16
+#"Misspelled Text\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 -1 -1 -1 0 0 0 0 0 0 0 0 0 1 1 1 0 0 0 0 0 0 -1 -1 2 1
+#"\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 81 112 203 0 0 0 -1 -1 2
+38 #"drracket:check-syntax:lexically-bound\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 81 112 203 0 0 0 -1 -1 2 1
+#"\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 178 34 34 0 0 0 -1 -1 2 28
+#"drracket:check-syntax:set!d\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 178 34 34 0 0 0 -1 -1 2 37
+#"drracket:check-syntax:unused-require\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 255 0 0 0 0 0 -1 -1 2 36
+#"drracket:check-syntax:free-variable\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 255 0 0 0 0 0 -1 -1 2 1
+#"\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 68 0 203 0 0 0 -1 -1 2 31
+#"drracket:check-syntax:imported\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 68 0 203 0 0 0 -1 -1 2 47
+#"drracket:check-syntax:my-obligation-style-pref\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 178 34 34 0 0 0 -1 -1 2 1
+#"\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 0 116 0 0 0 0 -1 -1 2 50
+#"drracket:check-syntax:their-obligation-style-pref\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 0 116 0 0 0 0 -1 -1 2 48
+#"drracket:check-syntax:unk-obligation-style-pref\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 0 0 0 0 0 0 -1 -1 2 1
+#"\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 139 142 28 0 0 0 -1 -1 2
+49 #"drracket:check-syntax:both-obligation-style-pref\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 139 142 28 0 0 0 -1 -1 2
+26 #"plt:htdp:test-coverage-on\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 0 0 0 0 0 0 -1 -1 2 1
+#"\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 93 -1 -1 0 1 0 0 0 1 0 0 0 0 0 0 255 165 0 0 0 0 -1 -1 2 27
+#"plt:htdp:test-coverage-off\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 93 -1 -1 0 1 0 0 0 1 0 0 0 0 0 0 255 165 0 0 0 0 -1 -1 4 1
+#"\0"
+0 70 1 #"\0"
+1.0 0 -1 -1 -1 -1 -1 -1 0 0 0 0 0 0 1.0 1.0 1.0 1.0 1.0 1.0 0 0 0 0 0 0
+-1 -1 4 4 #"XML\0"
+0 70 1 #"\0"
+1.0 0 -1 -1 -1 -1 -1 -1 0 0 0 0 0 0 1.0 1.0 1.0 1.0 1.0 1.0 0 0 0 0 0 0
+-1 -1 2 37 #"plt:module-language:test-coverage-on\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 0 0 0 0 0 0 -1 -1 2 38
+#"plt:module-language:test-coverage-off\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 93 -1 -1 0 1 0 0 0 1 0 0 0 0 0 0 255 165 0 0 0 0 -1 -1 4 1
+#"\0"
+0 71 1 #"\0"
+1.0 0 -1 -1 -1 -1 -1 -1 0 0 0 0 0 0 1.0 1.0 1.0 1.0 1.0 1.0 0 0 0 0 0 0
+-1 -1 4 1 #"\0"
+0 -1 1 #"\0"
+1.0 0 -1 -1 -1 -1 -1 -1 1 0 0 0 0 0 0 0 0 1.0 1.0 1.0 0 0 255 0 0 0 -1
+-1 4 1 #"\0"
+0 71 1 #"\0"
+1.0 0 -1 -1 -1 -1 -1 -1 1 0 0 0 0 0 0 0 0 1.0 1.0 1.0 0 0 255 0 0 0 -1
+-1 4 1 #"\0"
+0 71 1 #"\0"
+1.0 0 -1 -1 -1 -1 -1 -1 0 0 0 0 0 0 0 0 0 1.0 1.0 1.0 0 100 0 0 0 0 -1
+-1 4 1 #"\0"
+0 -1 1 #"\0"
+1.0 0 92 -1 -1 -1 -1 -1 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 255 255 0 -1 -1 0
+1 #"\0"
+0 75 12 #"Courier New\0"
+0.0 11 90 -1 90 -1 3 -1 0 1 0 1 0 0 0.0 0.0 0.0 0.0 0.0 0.0 0 0 0 255
+255 255 1 -1 2 1 #"\0"
+0 -1 1 #"\0"
+1.0 0 -1 -1 -1 -1 -1 -1 0 0 0 0 0 0 0.0 0.0 0.0 1.0 1.0 1.0 0 0 0 0 0 0
+-1 -1 2 1 #"\0"
+0 -1 1 #"\0"
+1.0 0 -1 -1 -1 93 -1 -1 0 1 0 0 0 0 0.0 0.0 0.0 1.0 1.0 1.0 150 0 150 0
+0 0 -1 -1 2 1 #"\0"
+0 -1 1 #"\0"
+1.0 0 -1 -1 93 -1 -1 -1 0 1 0 0 0 0 0.0 0.0 0.0 1.0 1.0 1.0 255 0 0 0 0
+0 -1 -1 2 1 #"\0"
+0 -1 1 #"\0"
+1.0 0 -1 -1 -1 93 -1 -1 0 1 0 0 0 0 0.0 0.0 0.0 1.0 1.0 1.0 0 0 175 0 0
+0 -1 -1 0 1 #"\0"
+0 75 12 #"Courier New\0"
+0.0 16 90 -1 90 -1 3 -1 0 1 0 1 0 0 0.0 0.0 0.0 0.0 0.0 0.0 0 0 0 255
+255 255 1 -1 2 1 #"\0"
+0 -1 1 #"\0"
+1.0 0 -1 -1 -1 -1 -1 -1 0 0 0 0 0 0 0.0 0.0 0.0 1.0 1.0 1.0 200 0 0 0 0
+0 -1 -1           0 4 9         325 21           0 0           0 53 0 14
+3 4 #"Type"
+0 0 24 3 1 #" "
+0 0 14 3 4 #"this"
+0 0 24 3 1 #" "
+0 0 14 3 2 #"in"
+0 0 24 3 1 #" "
+0 0 14 3 4 #"your"
+0 0 24 3 1 #" "
+0 0 14 3 5 #"other"
+0 0 24 3 1 #" "
+0 0 14 3 5 #"file."
+0 0 24 29 1 #"\n"
+0 0 24 29 1 #"\n"
+0 0 14 3 5 #"NOTE:"
+0 0 24 3 1 #" "
+0 0 14 3 4 #"this"
+0 0 24 3 1 #" "
+0 0 14 3 3 #"has"
+0 0 24 3 1 #" "
+0 0 14 3 2 #"to"
+0 0 24 3 1 #" "
+0 0 14 3 2 #"be"
+0 0 24 3 1 #" "
+0 0 14 3 5 #"AFTER"
+0 0 24 3 1 #" "
+0 0 14 3 3 #"all"
+0 0 24 3 1 #" "
+0 0 14 3 3 #"the"
+0 0 24 3 1 #" "
+0 0 21 3 1 #"'"
+0 0 15 3 12 #"define-block"
+0 0 21 3 1 #"'"
+0 0 24 29 1 #"\n"
+0 0 14 3 3 #"and"
+0 0 24 3 1 #" "
+0 0 21 3 1 #"'"
+0 0 15 3 11 #"define-item"
+0 0 21 3 1 #"'"
+0 0 24 3 1 #" "
+0 0 14 3 2 #"in"
+0 0 24 3 1 #" "
+0 0 14 3 4 #"your"
+0 0 24 3 1 #" "
+0 0 14 3 4 #"code"
+0 0 28 3 1 #","
+0 0 24 3 1 #" "
+0 0 14 3 3 #"but"
+0 0 24 3 1 #" "
+0 0 14 3 6 #"BEFORE"
+0 0 24 3 1 #" "
+0 0 21 3 1 #"'"
+0 0 14 3 11 #"compile-mod"
+0 0 21 3 1 #"'"
+0           0 0 0 24 29 1 #"\n"
+0 0 24 29 1 #"\n"
+0 15           4 21 7944
+(
+ #"(299.56875 73.60000000000001 5.4546875 16.9453125 ((do-set-pen! ((0 "
+ #"0 0 1.0) 1 solid round round #f)) (do-set-brush! ((255 255 255 1.0) "
+ #"solid #f #f #f)) (set-font (12 #f default normal normal #f default #"
+ #"f aligned)) (set-smoothing unsmoothed) (set-text-mode transparent) ("
+ #"set-alpha 1.0) (set-clipping-region #f) (set-background (255 255 255"
+ #" 1.0)) (set-text-background (255 255 255 1.0)) (set-text-foreground "
+ #"(0 0 0 1.0)) (set-smoothing aligned) (set-clipping-region (#t (((((0"
+ #".0 . 0.0) (299.56875 . 0.0) (299.56875 . 73.60000000000001) (0.0 . 7"
+ #"3.60000000000001)))) . any))) (transform #(0.4 0 0 0.4 0 0)) (transf"
+ #"orm #(4 0 0 4 0 0)) (do-set-pen! ((132 60 36 1.0) 1 solid round roun"
+ #"d #f)) (do-set-brush! ((132 60 36 1.0) solid #f #f #f)) (set-text-fo"
+ #"reground (132 60 36 1.0)) (set-font (12 #f modern normal bold #f def"
+ #"ault #t unaligned)) (draw-text \"(\" 0 0.0 #f 0 0) (set-font (12 #f "
+ #"default normal normal #f default #f aligned)) (do-set-pen! ((0 0 0 1"
+ #".0) 1 solid round round #f)) (do-set-brush! ((255 255 255 1.0) solid"
+ #" #f #f #f)) (set-text-foreground (0 0 0 1.0)) (do-set-pen! ((36 36 1"
+ #"40 1.0) 1 solid round round #f)) (do-set-brush! ((36 36 140 1.0) sol"
+ #"id #f #f #f)) (set-text-foreground (36 36 140 1.0)) (set-font (12 #f"
+ #" modern normal bold #f default #t unaligned)) (draw-text \"game-rule"
+ #"\" 7.201171875 0.0 #f 0 0) (set-font (12 #f default normal normal #f"
+ #" default #f aligned)) (do-set-pen! ((0 0 0 1.0) 1 solid round round "
+ #"#f)) (do-set-brush! ((255 255 255 1.0) solid #f #f #f)) (set-text-fo"
+ #"reground (0 0 0 1.0)) (set-font (12 #f modern normal bold #f default"
+ #" #t unaligned)) (draw-text \"   \" 0 16.0 #f 0 0) (set-font (12 #f d"
+ #"efault normal normal #f default #f aligned)) (do-set-pen! ((36 36 14"
+ #"0 1.0) 1 solid round round #f)) (do-set-brush! ((36 36 140 1.0) soli"
+ #"d #f #f #f)) (set-text-foreground (36 36 140 1.0)) (set-font (12 #f "
+ #"modern normal bold #f default #t unaligned)) (draw-text \"on:\" 21.6"
+ #"03515625 16.0 #f 0 0) (set-font (12 #f default normal normal #f defa"
+ #"ult #f aligned)) (do-set-pen! ((0 0 0 1.0) 1 solid round round #f)) "
+ #"(do-set-brush! ((255 255 255 1.0) solid #f #f #f)) (set-text-foregro"
+ #"und (0 0 0 1.0)) (set-font (12 #f modern normal bold #f default #t u"
+ #"naligned)) (draw-text \" \" 43.20703125 16.0 #f 0 0) (set-font (12 #"
+ #"f default normal normal #f default #f aligned)) (do-set-pen! ((36 36"
+ #" 140 1.0) 1 solid round round #f)) (do-set-brush! ((36 36 140 1.0) s"
+ #"olid #f #f #f)) (set-text-foreground (36 36 140 1.0)) (set-font (12 "
+ #"#f modern normal bold #f default #t unaligned)) (draw-text \"block\""
+ #" 50.408203125 16.0 #f 0 0) (set-font (12 #f default normal normal #f"
+ #" default #f aligned)) (do-set-pen! ((0 0 0 1.0) 1 solid round round "
+ #"#f)) (do-set-brush! ((255 255 255 1.0) solid #f #f #f)) (set-text-fo"
+ #"reground (0 0 0 1.0)) (set-font (12 #f modern normal bold #f default"
+ #" #t unaligned)) (draw-text \" \" 86.4140625 16.0 #f 0 0) (set-font ("
+ #"12 #f default normal normal #f default #f aligned)) (do-set-pen! ((3"
+ #"6 36 140 1.0) 1 solid round round #f)) (do-set-brush! ((36 36 140 1."
+ #"0) solid #f #f #f)) (set-text-foreground (36 36 140 1.0)) (set-font "
+ #"(12 #f modern normal bold #f default #t unaligned)) (draw-text \"pun"
+ #"ch\" 93.615234375 16.0 #f 0 0) (set-font (12 #f default normal norma"
+ #"l #f default #f aligned)) (do-set-pen! ((0 0 0 1.0) 1 solid round ro"
+ #"und #f)) (do-set-brush! ((255 255 255 1.0) solid #f #f #f)) (set-tex"
+ #"t-foreground (0 0 0 1.0)) (set-font (12 #f modern normal bold #f def"
+ #"ault #t unaligned)) (draw-text \" \" 129.62109375 16.0 #f 0 0) (set-"
+ #"font (12 #f default normal normal #f default #f aligned)) (do-set-pe"
+ #"n! ((36 36 140 1.0) 1 solid round round #f)) (do-set-brush! ((36 36 "
+ #"140 1.0) solid #f #f #f)) (set-text-foreground (36 36 140 1.0)) (set"
+ #"-font (12 #f modern normal bold #f default #t unaligned)) (draw-text"
+ #" \"xxx\" 136.822265625 16.0 #f 0 0) (set-font (12 #f default normal "
+ #"normal #f default #f aligned)) (do-set-pen! ((0 0 0 1.0) 1 solid rou"
+ #"nd round #f)) (do-set-brush! ((255 255 255 1.0) solid #f #f #f)) (se"
+ #"t-text-foreground (0 0 0 1.0)) (set-font (12 #f modern normal bold #"
+ #"f default #t unaligned)) (draw-text \"   \" 0 32.0 #f 0 0) (set-font"
+ #" (12 #f default normal normal #f default #f aligned)) (do-set-pen! ("
+ #"(36 36 140 1.0) 1 solid round round #f)) (do-set-brush! ((36 36 140 "
+ #"1.0) solid #f #f #f)) (set-text-foreground (36 36 140 1.0)) (set-fon"
+ #"t (12 #f modern normal bold #f default #t unaligned)) (draw-text \"d"
+ #"o:\" 21.603515625 32.0 #f 0 0) (set-font (12 #f default normal norma"
+ #"l #f default #f aligned)) (do-set-pen! ((0 0 0 1.0) 1 solid round ro"
+ #"und #f)) (do-set-brush! ((255 255 255 1.0) solid #f #f #f)) (set-tex"
+ #"t-foreground (0 0 0 1.0)) (set-font (12 #f modern normal bold #f def"
+ #"ault #t unaligned)) (draw-text \" \" 43.20703125 32.0 #f 0 0) (set-f"
+ #"ont (12 #f default normal normal #f default #f aligned)) (do-set-pen"
+ #"! ((132 60 36 1.0) 1 solid round round #f)) (do-set-brush! ((132 60 "
+ #"36 1.0) solid #f #f #f)) (set-text-foreground (132 60 36 1.0)) (set-"
+ #"font (12 #f modern normal bold #f default #t unaligned)) (draw-text "
+ #"\"(\" 50.408203125 32.0 #f 0 0) (set-font (12 #f default normal norm"
+ #"al #f default #f aligned)) (do-set-pen! ((0 0 0 1.0) 1 solid round r"
+ #"ound #f)) (do-set-brush! ((255 255 255 1.0) solid #f #f #f)) (set-te"
+ #"xt-foreground (0 0 0 1.0)) (do-set-pen! ((36 36 140 1.0) 1 solid rou"
+ #"nd round #f)) (do-set-brush! ((36 36 140 1.0) solid #f #f #f)) (set-"
+ #"text-foreground (36 36 140 1.0)) (set-font (12 #f modern normal bold"
+ #" #f default #t unaligned)) (draw-text \"particles\" 57.609375 32.0 #"
+ #"f 0 0) (set-font (12 #f default normal normal #f default #f aligned)"
+ #") (do-set-pen! ((0 0 0 1.0) 1 solid round round #f)) (do-set-brush! "
+ #"((255 255 255 1.0) solid #f #f #f)) (set-text-foreground (0 0 0 1.0)"
+ #") (set-font (12 #f modern normal bold #f default #t unaligned)) (dra"
+ #"w-text \" \" 122.419921875 32.0 #f 0 0) (set-font (12 #f default nor"
+ #"mal normal #f default #f aligned)) (do-set-pen! ((51 135 39 1.0) 1 s"
+ #"olid round round #f)) (do-set-brush! ((51 135 39 1.0) solid #f #f #f"
+ #")) (set-text-foreground (51 135 39 1.0)) (set-font (12 #f modern nor"
+ #"mal bold #f default #t unaligned)) (draw-text \"20\" 129.62109375 32"
+ #".0 #f 0 0) (set-font (12 #f default normal normal #f default #f alig"
+ #"ned)) (do-set-pen! ((0 0 0 1.0) 1 solid round round #f)) (do-set-bru"
+ #"sh! ((255 255 255 1.0) solid #f #f #f)) (set-text-foreground (0 0 0 "
+ #"1.0)) (set-font (12 #f modern normal bold #f default #t unaligned)) "
+ #"(draw-text \" \" 144.0234375 32.0 #f 0 0) (set-font (12 #f default n"
+ #"ormal normal #f default #f aligned)) (do-set-pen! ((36 36 140 1.0) 1"
+ #" solid round round #f)) (do-set-brush! ((36 36 140 1.0) solid #f #f "
+ #"#f)) (set-text-foreground (36 36 140 1.0)) (set-font (12 #f modern n"
+ #"ormal bold #f default #t unaligned)) (draw-text \"yyy\" 151.22460937"
+ #"5 32.0 #f 0 0) (set-font (12 #f default normal normal #f default #f "
+ #"aligned)) (do-set-pen! ((0 0 0 1.0) 1 solid round round #f)) (do-set"
+ #"-brush! ((255 255 255 1.0) solid #f #f #f)) (set-text-foreground (0 "
+ #"0 0 1.0)) (do-set-pen! ((132 60 36 1.0) 1 solid round round #f)) (do"
+ #"-set-brush! ((132 60 36 1.0) solid #f #f #f)) (set-text-foreground ("
+ #"132 60 36 1.0)) (set-font (12 #f modern normal bold #f default #t un"
+ #"aligned)) (draw-text \")\" 172.828125 32.0 #f 0 0) (set-font (12 #f "
+ #"default normal normal #f default #f aligned)) (do-set-pen! ((0 0 0 1"
+ #".0) 1 solid round round #f)) (do-set-brush! ((255 255 255 1.0) solid"
+ #" #f #f #f)) (set-text-foreground (0 0 0 1.0)) (do-set-pen! ((132 60 "
+ #"36 1.0) 1 solid round round #f)) (do-set-brush! ((132 60 36 1.0) sol"
+ #"id #f #f #f)) (set-text-foreground (132 60 36 1.0)) (set-font (12 #f"
+ #" modern normal bold #f default #t unaligned)) (draw-text \")\" 180.0"
+ #"29296875 32.0 #f 0 0) (set-font (12 #f default normal normal #f defa"
+ #"ult #f aligned)) (do-set-pen! ((0 0 0 1.0) 1 solid round round #f)) "
+ #"(do-set-brush! ((255 255 255 1.0) solid #f #f #f)) (set-text-foregro"
+ #"und (0 0 0 1.0)) (set-initial-matrix #(0.4 0.0 0.0 0.4 0.0 0.0)) (se"
+ #"t-origin 0.0 0.0) (set-scale 1.0 1.0) (set-rotation 0.0) (set-initia"
+ #"l-matrix #(1.0 0.0 0.0 1.0 0.0 0.0)) (set-origin 0.0 0.0) (set-scale"
+ #" 1.0 1.0) (set-rotation 0.0)))"
+) 0           0
